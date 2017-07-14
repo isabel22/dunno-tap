eeeefdf23b62cc8adc8f1c7079b3d292801894e6
Isabel Garcia <isabelgg22@gmail.com>
2017-07-14 22:35:37 +0200

    Removing icons and textures that we won't use in the game.

--- Assets/Scripts/Global.cs
+++ Assets/Scripts/Global.cs
@@ -44,26 +44,47 @@ public class Global : MonoBehaviour {
 		boxup = arrayofboxes[boxNumber];
 		string boxupName = boxup.gameObject.name;
 		counter = 0;
+		Vector3 visibleScale = new Vector3 (4.5f, 4.5f, 4.5f);
+		Vector3 visiblePosition = new Vector3 (0.0f, 0.0f, -0.5f);
 
 		//0
 		addPointSelected = GameObject.Find ("goldie" + boxupName);
-
 		//1
 		substPointSelected = GameObject.Find ("hammer" + boxupName);
+
+		int iconVisible = (int)Random.Range (0.4f, 1.4f);
 
 		while (counter < 45) {
 			boxup.gameObject.transform.transform.Rotate (Vector3.up * counter);
 			yield return  new WaitForSeconds (0.00000005f);
 			counter++;
 		}
+
+		if (iconVisible == 0) {
+			addPointSelected.transform.localScale = visibleScale;
+			addPointSelected.transform.localPosition = visiblePosition;
+		} else {
+			substPointSelected.transform.localScale = visibleScale;
+			substPointSelected.transform.localPosition = visiblePosition;
+		}
+
 		yield return  new WaitForSeconds (0.5f);
+
+		if (iconVisible == 0) {
+			addPointSelected.transform.localPosition = v0;
+			addPointSelected.transform.localScale = v0;
+		} else {
+			substPointSelected.transform.localPosition = v0;
+			substPointSelected.transform.localScale = v0;
+		}
+
 		counter = 0;
 		while (counter < 45) {
 			boxup.gameObject.transform.transform.Rotate (Vector3.down * counter);
 			yield return  new WaitForSeconds (0.00000005f);
 			counter++;
 		}
-		Debug.Log ("Name: " + boxup.gameObject.name);
+
 		boxTaken = false;
 	}
 }
