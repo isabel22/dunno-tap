1a9dc96fe3be364c33069beb6df7a27679667323
Isabel Garcia <isabelgg22@gmail.com>
2017-07-25 12:53:07 +0200

    Making more visible the Score Label.

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -6,9 +6,11 @@ using UnityEngine.UI;
 public class Character : MonoBehaviour {
 
 	public int value;
+	private static UnityEngine.UI.Text scoreLabel;
 
 	// Use this for initialization
 	public void Start () {
+		scoreLabel = GameObject.Find("Score").GetComponent<Text>();
 		this.GetComponent<Button> ().interactable = false;
 		this.resetValue ();
 	}
@@ -16,6 +18,7 @@ public class Character : MonoBehaviour {
 	public void UpdateScore(){
 		Global.score = Global.score + this.value;
 		this.resetValue ();
+		scoreLabel.SendMessage("VariableChangeHandler", scoreLabel.text);
 	}
 
 	public void updateValue(int newValue) {
---
+++ Assets/Scripts/Label.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Label : MonoBehaviour {
+
+	private Vector3 maxScale = new Vector3(2.4f, 2.4f, 2.4f);
+	private Vector3 basicScale = new Vector3(1.4f, 1.4f, 1.4f);
+
+	// Use this for initialization
+	void Start () {
+		GameObject.Find(this.name).GetComponent<Text>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void VariableChangeHandler(string newVal)
+	{
+		//make it bigger
+		StartCoroutine (enlargeChange ());
+	}
+		
+	IEnumerator enlargeChange() {
+		transform.localScale = maxScale;
+		yield return  new WaitForSeconds (0.5f);
+		transform.localScale = basicScale;
+	}
+}
